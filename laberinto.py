f3ae16de62545a22376fbca45e577ec78cb9f731
alumno <alumno@inforcytb04.(none)>
2018-03-09 12:40:16 +0100

    version final

--- laberinto.py
+++ laberinto.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy.random import random_integers as rand
 import matplotlib.pyplot as pyplot
 import matplotlib.animation as animation
+import copy as copy
 
 """Clase contenedora, Laberinto"""
 class Laberinto(object):
@@ -42,16 +43,16 @@ class Laberinto(object):
     complejidad: Parametro que controla la dificultad del laberinto generado.
     densidad:Parametro que controla la cantidad de paredes generadas.
     """
-    def generarlaberinto(self,columnas=15, filas=15, complejidad=0.95, densidad=4.75):
+    def generarlaberinto(self,columnas=100, filas=50, complejidad=0.95, densidad=0.75):
         if self.existelaberinto() == 'FALSE':
             
             """
-            Para generar solo matrices de dimensiones impares
+            Para generar solo matrices de dimensiones impares. Tupla que indican el numero de filas y columnas
             """   
             forma = ((filas // 2) * 2 + 1, (columnas // 2) * 2 + 1)
             
             """
-            Ajusta complejidad y densidad de acuerdo al tamano del laberinto
+            Ajusta complejidad y densidad de acuerdo al tamano del laberinto.
             """
             complejidad = int(complejidad * (5 * (forma[0] + forma[1])))
             densidad    = int(densidad * ((forma[0] // 2) * (forma[1] // 2)))
@@ -68,7 +69,7 @@ class Laberinto(object):
             self.data[0, :] = self.data[-1, :] = 1
             self.data[:, 0] = self.data[:, -1] = 1
             self.data[1, 1] = 3 
-	    self.data[-1, -2] = 5
+	    self.data[25, 0] = 5
             
             """
             Genero los pasillos
@@ -99,12 +100,12 @@ Clase Obstaculo, contenida en Laberinto
 class Obstaculo(object):
      
     """
-    Metodos clase obstaculo
+    Metodos clase obstaculo. 
     """   
     def __init__(self,pos,tipo): 
        
         """
-        Atributos del objeto Obstculo
+        Atributos del objeto Obstaculo. Posicion y tipo
         """      
         self.pos = pos
         self.tipo = tipo
@@ -114,12 +115,12 @@ class Obstaculo(object):
 class Robot(object):
    '''Clase generadora del objeto robot '''
    def __init__(self,pos):
-      '''Constructor del objeto robot, con los atributos posicion, frente (a donde mira), memoria '''
+      '''Constructor del objeto robot, con el atributo posicion que es una tupla que indica la posicion matricial del robot. (fila,columna)'''
       self.pos = pos
 
 
    def avanzar(self,pos,front):
-      '''Metodo avanzar'''
+      '''Metodo avanzar. Sus argumentos son la posicion del robot y el frente. Hace que el robot avance cambiando de posicion'''
      
       self.pos= np.add(pos,front)    
       return self.pos
@@ -128,61 +129,66 @@ class Robot(object):
         
 if __name__ == "__main__":
      
-  
+    i=0
     L=Laberinto()
     Laberinto.generarlaberinto(L)
-       # Sentencia que controla el color del arreglo
-#######################################################
+    a=[]
+    copy.deepcopy(a)
+    
     r=Robot([1,1])
     right=[0,1]
     left=[0,-1]
     up=[-1,0]
     down=[1,0]
     front= [0,1]
-    '''Asignacion de la posicion inicial (en forma matricial) y frente inicial (en forma cartesiana) del robot'''
-    while (L.data[tuple(np.add(r.pos,front))] != 5):
+    """Defino la posicion inicial del robot, la derecha, iaquierda arriba, abajo y frente como una tupla"""
+
+    while (L.data[tuple(np.add(r.pos,up))] != 5 and L.data[tuple(np.add(r.pos,down))] != 5 and L.data[tuple(np.add(r.pos,right))] != 5 and L.data[tuple(np.add(r.pos,left))] != 5):
       ''' Criterios para tomar decicionens y buscar la salida'''
-      if(L.data[tuple(np.add(r.pos,up))]==1 and (L.data[tuple(np.add(r.pos,right))]==0 or L.data[tuple(np.add(r.pos,right))]==3) and front == right):
+      if(L.data[tuple(np.add(r.pos,up))]==1 and (L.data[tuple(np.add(r.pos,right))]==0 or L.data[tuple(np.add(r.pos,right))]==3 or L.data[tuple(np.add(r.pos,right))]==5) and front == right):
            L.data[tuple(r.avanzar(r.pos,front))] = 3
-      elif (L.data[tuple(np.add(r.pos,right))] == 1 and (L.data[tuple(np.add(r.pos , down))] == 0 or L.data[tuple(np.add(r.pos,down))]==3) and front == down):
+      elif (L.data[tuple(np.add(r.pos,right))] == 1 and (L.data[tuple(np.add(r.pos , down))] == 0 or L.data[tuple(np.add(r.pos,down))]==3 or L.data[tuple(np.add(r.pos,down))]==5) and front == down):
            L.data[tuple(r.avanzar(r.pos,front))]=3
-      elif(L.data[tuple(np.add(r.pos,down))] == 1 and (L.data[tuple(np.add(r.pos ,left))] == 0 or L.data[tuple(np.add(r.pos,left))]==3) and front == left):
+      elif(L.data[tuple(np.add(r.pos,down))] == 1 and (L.data[tuple(np.add(r.pos ,left))] == 0 or L.data[tuple(np.add(r.pos,left))]==3 or L.data[tuple(np.add(r.pos,left))]==5) and front == left):
            L.data[tuple(r.avanzar(r.pos,front))]=3
-      elif(L.data[tuple(np.add(r.pos,left))] == 1 and (L.data[tuple(np.add(r.pos ,up))] == 0 or L.data[tuple(np.add(r.pos,up))]==3) and front == up):
+      elif(L.data[tuple(np.add(r.pos,left))] == 1 and (L.data[tuple(np.add(r.pos ,up))] == 0 or L.data[tuple(np.add(r.pos,up))]==3 or L.data[tuple(np.add(r.pos,up))]==5) and front == up):
            L.data[tuple(r.avanzar(r.pos,front))]=3
-      elif (L.data[tuple(np.add(r.pos,up))] == 1 and (L.data[tuple(np.add(r.pos,right))] == 1 or L.data[tuple(np.add(r.pos,right))] == 3) and front == right):
+      elif (L.data[tuple(np.add(r.pos,up))] == 1 and L.data[tuple(np.add(r.pos,right))] == 1 and front == right):
             front = down 
-      elif (L.data[tuple(np.add(r.pos,right))] == 1 and (L.data[tuple(np.add(r.pos ,down))] == 1 or L.data[tuple(np.add(r.pos,down))] == 3)  and front ==down):
+      elif (L.data[tuple(np.add(r.pos,right))] == 1 and L.data[tuple(np.add(r.pos ,down))] == 1  and front ==down):
             front = left
-      elif (L.data[tuple(np.add(r.pos,down))] == 1 and (L.data[tuple(np.add(r.pos,left))] == 1 or L.data[tuple(np.add(r.pos,left))] == 3) and front==left):
+      elif (L.data[tuple(np.add(r.pos,down))] == 1 and L.data[tuple(np.add(r.pos,left))] == 1 and front==left):
             front = up
-      elif  (L.data[tuple(np.add(r.pos,left))] == 1 and (L.data[tuple(np.add(r.pos, up))] == 1 or L.data[tuple(np.add(r.pos,up))] == 3) and front == up):
+      elif  (L.data[tuple(np.add(r.pos,left))] == 1 and L.data[tuple(np.add(r.pos, up))] == 1 and front == up):
             front = right
-###########################################################
-      elif (L.data[tuple(np.add(r.pos,right))] == 0 and (L.data[tuple(np.add(r.pos,left))] == 1 or L.data[tuple(np.add(r.pos,left))] == 3) and front == down):
-            front = right
-      elif (L.data[tuple(np.add(r.pos,left))] == 3 and (L.data[tuple(np.add(r.pos,up))] == 1 or L.data[tuple(np.add(r.pos,left))] == 3) and front == up):
-            front = left   
+      elif ((L.data[tuple(np.add(r.pos,up))] == 0 or L.data[tuple(np.add(r.pos,up))] == 3 or L.data[tuple(np.add(r.pos,up))] == 5) and (L.data[tuple(np.add(r.pos,right))] == 1 or L.data[tuple(np.add(r.pos,right))]==0 or L.data[tuple(np.add(r.pos,right))]==3) and front == right):
+            front = up 
+            L.data[tuple(r.avanzar(r.pos,front))]=3
+      elif ((L.data[tuple(np.add(r.pos,right))] == 0 or L.data[tuple(np.add(r.pos,right))] == 3 or L.data[tuple(np.add(r.pos,right))] == 5) and (L.data[tuple(np.add(r.pos,down))] == 1 or L.data[tuple(np.add(r.pos,down))] == 0 or L.data[tuple(np.add(r.pos,down))] == 3) and front == down):
+            front = right 
+            L.data[tuple(r.avanzar(r.pos,front))]=3
+      elif ((L.data[tuple(np.add(r.pos,down))] == 0 or L.data[tuple(np.add(r.pos,down))] == 3 or L.data[tuple(np.add(r.pos,down))] == 5) and (L.data[tuple(np.add(r.pos,left))] == 1 or L.data[tuple(np.add(r.pos,left))] == 0 or L.data[tuple(np.add(r.pos,left))] == 3) and front == left):
+            front = down 
+            L.data[tuple(r.avanzar(r.pos,front))]=3
+      elif ((L.data[tuple(np.add(r.pos,left))] == 0 or L.data[tuple(np.add(r.pos,left))] == 3 or L.data[tuple(np.add(r.pos,left))] == 5) and (L.data[tuple(np.add(r.pos,up))] == 1 or L.data[tuple(np.add(r.pos,up))] == 0 or L.data[tuple(np.add(r.pos,up))] == 3)   and front == up):
+            front = left 
+            L.data[tuple(r.avanzar(r.pos,front))]=3
       
-###########################################################
+
       else: print "Esta todo considerado (?)"
-      print L.data
-      print r.pos, front 
-      print L.data[tuple(np.add(r.pos,up))],L.data[tuple(np.add(r.pos,right))]
-      #print L.data
-      #print r.pos
+      '''Metodo para que el robot encuentre la salida del laberinto, si a la hora de tomar la decision ocurre una circunstancia no considerada imprime error'''
+      a.append(copy.deepcopy(L.data))
 
-  
-########################################################
 
-    def func_variacion(frame):    
-        
-        matriz.set_array(L.data)
+
+
+    def func_variacion(frame):
+        matriz.set_array(a[frame])
     fig, ax = pyplot.subplots()
-    matriz = ax.matshow(L.data)
-    ani = animation.FuncAnimation(fig, func_variacion, frames=10, interval=1500, repeat=False)   
+    matriz = ax.matshow(a[0])
+    
+    ani = animation.FuncAnimation(fig, func_variacion, frames=10000, interval=50, repeat=False)   
 
-        # Muestra un arreglo como una matriz en una nueva figura
 
           
 
